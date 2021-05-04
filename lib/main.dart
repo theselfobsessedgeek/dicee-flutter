72aa852bd5c80a49ca99b770c18a73f84f4ec91d
theselfobsessedgeek <shubhamahlawatg@gmail.com>
2021-05-04 22:47:30 +0530

    Finally Done.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,10 +16,48 @@ void main() {
     ),
   );
 }
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
 
-class DicePage extends StatelessWidget {
+class _DicePageState extends State<DicePage> {
+  var lnum = 1;
+  var rnum =1;
+  void changeNum(){
+    lnum= Random().nextInt(6)+1;
+    rnum= Random().nextInt(6)+1;
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(child: TextButton(
+            onPressed: (){
+              setState(() {
+                changeNum();
+                print("Button 1 dab gya ...");
+              });
+          },
+            child: Image.asset('images/dice$lnum.png'),
+
+          )),
+          Expanded(child: TextButton(
+            onPressed: (){
+              setState(() {
+                changeNum();
+                print("Button dab gya 2...");
+              });
+            },
+            child: Image.asset('images/dice$rnum.png'),
+
+          ))
+
+        ],
+      ),
+    );
+
   }
 }
+
